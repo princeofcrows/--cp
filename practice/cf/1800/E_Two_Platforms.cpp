42ad6121fb37f43f9d312b2342b885d5194b2c63
prince_of_crows <tamjidjasir@gmail.com>
2023-09-28 03:04:15 +0600

    solve: cf ****/E

---
+++ practice/cf/1800/E_Two_Platforms.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Data type
+#define int long long
+#define ll long long
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pii>
+#define mii map<int, int>
+#define msi map<string, int>
+#define si set<int>
+#define spi set<pii>
+
+// Method or var shorthands
+#define fi first
+#define se second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define sort_all(v) sort(all(v));
+#define rev_all(v) reverse(all(v));
+#define mem(ara, x) memset(ara, x, sizeof ara)
+
+// Const
+#define mod 1000000007
+// 28722900390631
+#define inf 1e18 + 19
+#define mx 200015
+#define pi acos(-1.0)
+#define seed 997
+// 769 919 647 839
+
+// Input Output
+#define sild(x) scanf("%lld", &x)
+#define sid(x) scanf("%d", &x)
+#define rin() freopen("in.txt", "r", stdin)
+#define wrout() freopen("out.txt", "w", stdout)
+#define fst                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Loops
+#define fr(i, s, e) for (int i = s; i < e; i++)
+#define ifr(i, s, e) for (int i = s; i <= e; i++)
+#define rfr(i, e, s) for (int i = e - 1; i >= s; i--)
+
+// Functions
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+int Set(int N, int pos) { return N = N | (1 << pos); }
+int reset(int N, int pos) { return N = N & ~(1 << pos); }
+bool check(int N, int pos) { return (bool)(N & (1 << pos)); }
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+
+int dp[mx][2], x[mx], y[mx];
+
+int solve(int idx, int n, int f, int k)
+{
+    if (idx >= n || f > 1)
+    {
+        return 0;
+    }
+    if (dp[idx][f] != -1)
+    {
+        return dp[idx][f];
+    }
+
+    int lx = upper_bound(x + idx, x + n, x[idx] + k) - x;
+    int ret1 = solve(lx, n, f + 1, k) + (lx - idx);
+    int ret2 = solve(idx + 1, n, f, k);
+
+    return dp[idx][f] = max(ret1, ret2);
+}
+
+int32_t main()
+{
+    // rin();
+    // wrout();
+    fst;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        fr(i, 0, n) cin >> x[i];
+        fr(i, 0, n) cin >> y[i];
+
+        sort(x, x + n);
+
+        fr(j, 0, 2)
+            fr(i, 0, n) dp[i][j] = -1;
+
+        cout << solve(0, n, 0, k) << endl;
+    }
+    return 0;
+}
