03863aacdf534a05042f8bc5c36095096d4e73fe
prince_of_crows <tamjidjasir@gmail.com>
2023-10-09 05:58:44 +0600

    Formatted Template

--- template.cpp
+++ template.cpp
@@ -5,8 +5,8 @@ using namespace std;
 // Data type
 #define int long long
 #define ll long long
-#define pii pair <int, int>
-#define piii pair <int, pii>
+#define pii pair<int, int>
+#define piii pair<int, pii>
 #define vi vector<int>
 #define vll vector<long long>
 #define vpi vector<pii>
@@ -29,7 +29,7 @@ using namespace std;
 // Const
 #define mod 1000000007
 // 28722900390631
-#define inf 1e18+19
+#define inf 1e18 + 19
 #define mx 200015
 #define pi acos(-1.0)
 #define seed 997
@@ -40,32 +40,41 @@ using namespace std;
 #define sid(x) scanf("%d", &x)
 #define rin() freopen("in.txt", "r", stdin)
 #define wrout() freopen("out.txt", "w", stdout)
-#define fst ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fst                       \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);                   \
+	cout.tie(0);
 
 // Loops
-#define fr(i,s,e) for(int i=s; i<e; i++)
-#define ifr(i,s,e) for(int i=s; i<=e; i++)
-#define rfr(i,e,s) for(int i=e-1; i>=s; i--)
+#define fr(i, s, e) for (int i = s; i < e; i++)
+#define ifr(i, s, e) for (int i = s; i <= e; i++)
+#define rfr(i, e, s) for (int i = e - 1; i >= s; i--)
 
 // Functions
-ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
-ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
-int Set(int N,int pos) { return N=N | (1<<pos); }
-int reset(int N,int pos) { return N= N & ~(1<<pos); }
-bool check(int N,int pos){ return (bool)(N & (1<<pos)); }
+ll gcd(ll a, ll b)
+{
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+int Set(int N, int pos) { return N = N | (1 << pos); }
+int reset(int N, int pos) { return N = N & ~(1 << pos); }
+bool check(int N, int pos) { return (bool)(N & (1 << pos)); }
 void yes() { cout << "YES\n"; }
 void no() { cout << "NO\n"; }
 
-int32_t main(){
-	//rin();
-	//wrout();
+int32_t main()
+{
+	// rin();
+	// wrout();
 	fst;
 
 	int t;
 	cin >> t;
 
-	while(t--) {
-        
+	while (t--)
+	{
 	}
 	return 0;
 }
