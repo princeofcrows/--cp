f3c999ae264993c49da31374255cfe46e7c93d7f
prince_of_crows <tamjidjasir@gmail.com>
2023-10-28 03:15:23 +0600

    161 D

---
+++ practice/cf/1800/D_Distance_in_Tree.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Data type
+#define int long long
+#define ll long long
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pii>
+#define mii map<int, int>
+#define msi map<string, int>
+#define si set<int>
+#define spi set<pii>
+
+// Method or var shorthands
+#define fi first
+#define se second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define sort_all(v) sort(all(v));
+#define rev_all(v) reverse(all(v));
+#define mem(ara, x) memset(ara, x, sizeof ara)
+
+// Const
+#define mod 1000000007
+// 28722900390631
+#define inf 1e18 + 19
+#define mx 200015
+#define pi acos(-1.0)
+#define seed 997
+// 769 919 647 839
+
+// Input Output
+#define sild(x) scanf("%lld", &x)
+#define sid(x) scanf("%d", &x)
+#define rin() freopen("in.txt", "r", stdin)
+#define wrout() freopen("out.txt", "w", stdout)
+#define fst                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Loops
+#define fr(i, s, e) for (int i = s; i < e; i++)
+#define ifr(i, s, e) for (int i = s; i <= e; i++)
+#define rfr(i, e, s) for (int i = e - 1; i >= s; i--)
+
+// Functions
+ll gcd(ll a, ll b) { return b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+int Set(int N, int pos) { return N = N | (1 << pos); }
+int reset(int N, int pos) { return N = N & ~(1 << pos); }
+bool check(int N, int pos) { return (bool)(N & (1 << pos)); }
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+
+// random
+mt19937 rng(chrono::steady_clock().now().time_since_epoch().count());
+
+vi chain;
+int n, k, res[502][50005], ans = 0;
+
+vi g[50005];
+
+void solve(int u, int p)
+{
+    chain.pb(u);
+
+    int idx = chain.size() - 1;
+    int lev = 0;
+    while (lev <= k && idx >= 0)
+    {
+        res[lev][chain[idx]]++;
+        lev++;
+        idx--;
+    }
+
+    for (auto v : g[u])
+    {
+        if (p != v)
+        {
+            solve(v, u);
+
+            fr(i, 1, k)
+            {
+                int x = res[i - 1][v] * (res[k - i][u] - res[k - i - 1][v]);
+
+                ans += max(0LL, x);
+            }
+        }
+    }
+
+    ans += res[k][u];
+    chain.pop_back();
+}
+
+int32_t main()
+{
+    // rin();
+    // wrout();
+    fst;
+
+    cin >> n >> k;
+
+    fr(i, 0, n - 1)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        g[a].pb(b);
+        g[b].pb(a);
+    }
+
+    mem(res, 0);
+
+    solve(1, -1);
+
+    cout << ans << endl;
+    return 0;
+}
