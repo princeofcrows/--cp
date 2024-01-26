3fa4c6f1e597901f8a2d2152f22686651ab2598d
prince_of_crows <tamjidjasir@gmail.com>
2024-01-27 01:45:21 +0600

    solve palindrome game

---
+++ practice/cf/1900/B_2_Palindrome_Game_hard_version.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Data type
+#define int long long
+#define ll long long
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pii>
+#define mii map<int, int>
+#define msi map<string, int>
+#define si set<int>
+#define spi set<pii>
+
+// Method or var shorthands
+#define fi first
+#define se second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define sort_all(v) sort(all(v));
+#define rev_all(v) reverse(all(v));
+#define mem(ara, x) memset(ara, x, sizeof ara)
+
+// Const
+#define mod 1000000007
+// 28722900390631
+#define inf 1e18 + 19
+#define mx 200015
+#define pi acos(-1.0)
+#define seed 997
+// 769 919 647 839
+
+// Input Output
+#define sild(x) scanf("%lld", &x)
+#define sid(x) scanf("%d", &x)
+#define rin() freopen("in.txt", "r", stdin)
+#define wrout() freopen("out.txt", "w", stdout)
+#define fst                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Loops
+#define fr(i, s, e) for (int i = s; i < e; i++)
+#define ifr(i, s, e) for (int i = s; i <= e; i++)
+#define rfr(i, e, s) for (int i = e - 1; i >= s; i--)
+
+// Functions
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+int Set(int N, int pos) { return N = N | (1 << pos); }
+int reset(int N, int pos) { return N = N & ~(1 << pos); }
+bool check(int N, int pos) { return (bool)(N & (1 << pos)); }
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+
+vi un, ev;
+
+int32_t main()
+{
+    // rin();
+    // wrout();
+    fst;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, u = 0, e = 0;
+        string s;
+
+        cin >> n >> s;
+
+        int i = 0, j = n - 1;
+        while (i <= j)
+        {
+            if (s[i] != s[j])
+            {
+                u++;
+            }
+            else if (s[i] == '0')
+            {
+                e++;
+                if (i != j)
+                {
+                    e++;
+                }
+            }
+            i++;
+            j--;
+        }
+
+        int bobSpent, aliceSpent;
+
+        if (u == 0)
+        {
+            if (e % 2)
+            {
+                if (e == 1)
+                {
+                    bobSpent = 0, aliceSpent = 1;
+                }
+                else
+                {
+                    bobSpent = 1, aliceSpent = 0;
+                }
+            }
+            else
+            {
+                bobSpent = 0, aliceSpent = 1;
+            }
+        }
+        else
+        {
+            if (e == 1 && u == 1)
+            {
+                bobSpent = 0;
+                aliceSpent = 0;
+            }
+            else
+            {
+                bobSpent = 1;
+                aliceSpent = 0;
+            }
+        }
+
+        if (bobSpent == aliceSpent)
+        {
+            cout << "DRAW\n";
+        }
+        else if (bobSpent < aliceSpent)
+        {
+            cout << "BOB\n";
+        }
+        else
+        {
+            cout << "ALICE\n";
+        }
+    }
+    return 0;
+}
