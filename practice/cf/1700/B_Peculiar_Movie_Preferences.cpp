b287e7c98bcda857e5ce11fcd326921620c0688e
prince_of_crows <tamjidjasir@gmail.com>
2025-11-15 01:28:29 +0600

    Implement solution for B_Peculiar_Movie_Preferences problem

---
+++ practice/cf/1700/B_Peculiar_Movie_Preferences.cpp
@@ -0,0 +1,180 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Data type
+#define int long long
+#define ll long long
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pii>
+#define mii map<int, int>
+#define msi map<string, int>
+#define si set<int>
+#define spi set<pii>
+
+// Method or var shorthands
+#define fi first
+#define se second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define sort_all(v) sort(all(v));
+#define rev_all(v) reverse(all(v));
+#define mem(ara, x) memset(ara, x, sizeof ara)
+
+// Input Output
+#define sild(x) scanf("%lld", &x)
+#define sid(x) scanf("%d", &x)
+#define rin() freopen("in.txt", "r", stdin)
+#define wrout() freopen("out.txt", "w", stdout)
+#define fst                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Loops
+#define fr(i, s, e) for (int i = s; i < e; i++)
+#define ifr(i, s, e) for (int i = s; i <= e; i++)
+#define rfr(i, e, s) for (int i = e - 1; i >= s; i--)
+
+// Functions
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+int Set(int N, int pos) { return N = N | (1 << pos); }
+int reset(int N, int pos) { return N = N & ~(1 << pos); }
+bool check(int N, int pos) { return (bool)(N & (1 << pos)); }
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+
+// Const
+const int mod = 1000000007;
+// 28722900390631
+const int inf = 1e18 + 19;
+const int MX = 200015;
+const int pi = acos(-1.0);
+const int seed = 997;
+// 769 919 647 839
+
+string s[MX];
+int n;
+
+bool checkInd()
+{
+    fr(i, 0, n)
+    {
+        int lst = s[i].length() - 1;
+        if (s[i][0] == s[i][lst])
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool check23()
+{
+    map<string, bool> mp2, mp3;
+    fr(i, 0, n)
+    {
+        int lst = s[i].length() - 1;
+        string chk = s[i].substr(lst - 1, 2);
+        rev_all(chk);
+
+        if (s[i].length() == 2)
+        {
+            if (mp2[chk] || mp3[chk])
+            {
+                return true;
+            }
+        }
+        else if (s[i].length() == 3)
+        {
+            if (mp2[chk])
+            {
+                return true;
+            }
+        }
+
+        string rev = s[i].substr(0, 2);
+
+        if (s[i].length() == 2)
+        {
+            mp2[rev] = true;
+        }
+        else if (s[i].length() == 3)
+        {
+            mp3[rev] = true;
+        }
+    }
+
+    return false;
+}
+
+bool check33()
+{
+    map<string, bool> mp3;
+    fr(i, 0, n)
+    {
+        if (s[i].length() != 3)
+        {
+            continue;
+        }
+        string chk = s[i];
+        rev_all(chk);
+
+        if (mp3[chk])
+        {
+            return true;
+        }
+
+        mp3[s[i]] = true;
+    }
+
+    return false;
+}
+
+int32_t main()
+{
+    // rin();
+    // wrout();
+    fst;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n;
+
+        fr(i, 0, n)
+        {
+            cin >> s[i];
+        }
+
+        if (!checkInd())
+        {
+            if (check23())
+            {
+                yes();
+            }
+            else if (check33())
+            {
+                yes();
+            }
+            else
+            {
+                no();
+            }
+        }
+        else
+        {
+            yes();
+        }
+    }
+    return 0;
+}
