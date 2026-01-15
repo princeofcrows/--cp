9f6189140540f32a7b6a25ce867de68a93ddde94
prince_of_crows <tamjidjasir@gmail.com>
2026-01-15 21:37:15 +0600

    Implement solutions for A_Octave, B_Trifecta, C_Striped_Horse, and D_Forbidden_List_2 problems

---
+++ contest/atcoder/abc-440/D_Forbidden_List_2.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Data type
+#define int long long
+#define ll long long
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pii>
+#define mii map<int, int>
+#define msi map<string, int>
+#define si set<int>
+#define spi set<pii>
+
+// Method or var shorthands
+#define fi first
+#define se second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define sort_all(v) sort(all(v));
+#define rev_all(v) reverse(all(v));
+#define mem(ara, x) memset(ara, x, sizeof ara)
+
+// Input Output
+#define sild(x) scanf("%lld", &x)
+#define sid(x) scanf("%d", &x)
+#define rin() freopen("in.txt", "r", stdin)
+#define wrout() freopen("out.txt", "w", stdout)
+#define fst                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Loops
+#define fr(i, s, e) for (int i = s; i < e; i++)
+#define ifr(i, s, e) for (int i = s; i <= e; i++)
+#define rfr(i, e, s) for (int i = e - 1; i >= s; i--)
+
+// Functions
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+int Set(int N, int pos) { return N = N | (1 << pos); }
+int reset(int N, int pos) { return N = N & ~(1 << pos); }
+bool check(int N, int pos) { return (bool)(N & (1 << pos)); }
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+
+// Const
+const int mod = 1000000007;
+// 28722900390631
+const int inf = 1e18 + 19;
+const int MX = 300015;
+const int pi = acos(-1.0);
+const int seed = 997;
+// 769 919 647 839
+
+int a[MX], res[MX], cnt[MX];
+vector<piii> v;
+
+int32_t main()
+{
+    // rin();
+    // wrout();
+    fst;
+
+    int n, q;
+    cin >> n >> q;
+
+    a[0] = 0;
+    ifr(i, 1, n)
+    {
+        cin >> a[i];
+    }
+
+    sort(a, a + n + 1);
+
+    cnt[0] = 0;
+    ifr(i, 1, n)
+    {
+        cnt[i] = a[i] - a[i - 1] - 1;
+        cnt[i] += cnt[i - 1];
+    }
+
+    cnt[n + 1] = inf;
+
+    fr(i, 0, q)
+    {
+        int x, y;
+        cin >> x >> y;
+        v.pb({x, {y, i}});
+    }
+
+    sort_all(v);
+
+    int cur = 0;
+    fr(i, 0, q)
+    {
+        while (cur <= n && a[cur] < v[i].fi)
+        {
+            cur++;
+        }
+
+        if (cur > n)
+        {
+            res[v[i].se.se] = v[i].fi + v[i].se.fi - 1;
+            // cout << v[i].se.se << " " << res[v[i].se.se] << endl;
+            continue;
+        }
+
+        int idx = lower_bound(cnt, cnt + n + 2, cnt[cur - 1] + v[i].se.se) - cnt;
+        res[v[i].se.se] = a[idx - 1] + v[i].se.fi;
+        cout << v[i].se.se << " " << res[v[i].se.se] << " " << cur << " " << idx << endl;
+    }
+
+    fr(i, 0, q)
+    {
+        cout << res[i] << endl;
+    }
+
+    return 0;
+}
