c541d9f94ee6ed975d7ba8a779ca2b010eb4d59e
prince_of_crows <tamjidjasir@gmail.com>
2026-01-17 19:58:54 +0600

    Implement solutions for A_Black_Square, B_Two_Languages, C_Sake_or_Water, D_Paid_Walk, and E_A_B_substring problems

---
+++ contest/atcoder/abc-441/E_A_B_substring.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Data type
+#define int long long
+#define ll long long
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pii>
+#define mii map<int, int>
+#define msi map<string, int>
+#define si set<int>
+#define spi set<pii>
+
+// Method or var shorthands
+#define fi first
+#define se second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define sort_all(v) sort(all(v));
+#define rev_all(v) reverse(all(v));
+#define mem(ara, x) memset(ara, x, sizeof ara)
+
+// Input Output
+#define sild(x) scanf("%lld", &x)
+#define sid(x) scanf("%d", &x)
+#define rin() freopen("in.txt", "r", stdin)
+#define wrout() freopen("out.txt", "w", stdout)
+#define fst                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Loops
+#define fr(i, s, e) for (int i = s; i < e; i++)
+#define ifr(i, s, e) for (int i = s; i <= e; i++)
+#define rfr(i, e, s) for (int i = e - 1; i >= s; i--)
+
+// Functions
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+int Set(int N, int pos) { return N = N | (1 << pos); }
+int reset(int N, int pos) { return N = N & ~(1 << pos); }
+bool check(int N, int pos) { return (bool)(N & (1 << pos)); }
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+
+// Const
+const int mod = 1000000007;
+// 28722900390631
+const int inf = 1e18 + 19;
+const int MX = 500015;
+const int pi = acos(-1.0);
+const int seed = 997;
+// 769 919 647 839
+
+int a[MX], b[MX], ab[MX];
+
+const int mx = MX * 2;
+int tree[mx + 5];
+
+int read(int idx)
+{
+    int sum = 0;
+    while (idx > 0)
+    {
+        sum += tree[idx];
+        idx -= (idx & -idx);
+    }
+    return sum;
+}
+
+void update(int idx, int val)
+{
+    while (idx <= mx)
+    {
+        tree[idx] += val;
+        idx += (idx & -idx);
+    }
+}
+
+int32_t main()
+{
+    // rin();
+    // wrout();
+    fst;
+
+    int n, ans = 0;
+    string s;
+    cin >> n >> s;
+
+    mem(a, 0);
+    mem(b, 0);
+    mem(ab, 0);
+    mem(tree, 0);
+
+    int co = n + 3;
+    update(co, 1);
+    fr(i, 0, n)
+    {
+        if (s[i] == 'A')
+        {
+            a[i + 1]++;
+        }
+        if (s[i] == 'B')
+        {
+            b[i + 1]++;
+        }
+
+        a[i + 1] += a[i];
+        b[i + 1] += b[i];
+
+        ab[i + 1] = a[i + 1] - b[i + 1];
+        // ab[i + 1] += ab[i];
+        update(ab[i + 1] + co, 1);
+
+        int x = co + ab[i + 1] - 1;
+        ans += (read(x));
+
+        // cout << i + 1 << " " << ab[i + 1] << " " << (read(x))
+        //      << endl;
+    }
+
+    cout << ans;
+    return 0;
+}
