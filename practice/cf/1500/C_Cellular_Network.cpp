c11c84f631c1da12d1ebd00687bc8950c04dbf19
Tamjid Jasir <tamjid.jasir@optimizely.com>
2026-02-09 18:12:08 +0600

    Add implementation for C_Cellular_Network problem

---
+++ practice/cf/1500/C_Cellular_Network.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Data type
+#define int long long
+#define ll long long
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pii>
+#define mii map<int, int>
+#define msi map<string, int>
+#define si set<int>
+#define spi set<pii>
+
+// Method or var shorthands
+#define fi first
+#define se second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define sort_all(v) sort(all(v));
+#define rev_all(v) reverse(all(v));
+#define mem(ara, x) memset(ara, x, sizeof ara)
+
+
+
+// Input Output
+#define sild(x) scanf("%lld", &x)
+#define sid(x) scanf("%d", &x)
+#define rin() freopen("in.txt", "r", stdin)
+#define wrout() freopen("out.txt", "w", stdout)
+#define fst                       \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);                   \
+	cout.tie(0);
+
+// Loops
+#define fr(i, s, e) for (int i = s; i < e; i++)
+#define ifr(i, s, e) for (int i = s; i <= e; i++)
+#define rfr(i, e, s) for (int i = e - 1; i >= s; i--)
+
+// Functions
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+int Set(int N, int pos) { return N = N | (1 << pos); }
+int reset(int N, int pos) { return N = N & ~(1 << pos); }
+bool check(int N, int pos) { return (bool)(N & (1 << pos)); }
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+
+// Const
+const int mod = 1000000007;
+// 28722900390631
+const int inf = 1e18 + 19;
+const int MX = 200015;
+const int pi = acos(-1.0);
+const int seed = 997;
+// 769 919 647 839
+
+int a[MX], b[MX];
+
+int32_t main()
+{
+    // rin();
+    // wrout();
+    fst;
+
+    int n, m; cin >> n >> m;
+
+    si s;
+
+    fr(i, 0, n) {
+        cin >> a[i];
+
+    }
+
+    int ans = 0;
+    fr(i, 0, m) {
+        cin >> b[i];
+        s.insert(b[i]);
+    }
+
+    fr(i, 0, n) {
+        auto it = s.upper_bound(a[i]);
+        int r1 = 0, r2 = 0;
+        if (it != s.end()) {
+            r1 = max(r1, llabs(a[i] - (*it)));
+        }
+        else r1 = inf;
+
+        if (it != s.begin()) {
+            --it;
+            r2 = max(r2, llabs(a[i] - (*it)));
+        }
+        else r2 = inf;
+
+        ans = max(ans, min(r1, r2));
+    }
+
+    cout << ans << endl;
+    return 0;
+}
