843e097a17709e1f78ca569dc71a0366c1442308
prince_of_crows <tamjidjasir@gmail.com>
2026-04-14 04:25:53 +0600

    Add implementation for E_Gardener_and_Tree problem

---
+++ practice/cf/1600/E_Gardener_and_Tree.cpp
@@ -0,0 +1,150 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Data type
+// #define int long long
+#define ll long long
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pii>
+#define mii map<int, int>
+#define msi map<string, int>
+#define si set<int>
+#define spi set<pii>
+
+// Method or var shorthands
+#define fi first
+#define se second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define sort_all(v) sort(all(v));
+#define rev_all(v) reverse(all(v));
+#define mem(ara, x) memset(ara, x, sizeof ara)
+
+// Input Output
+#define sild(x) scanf("%lld", &x)
+#define sid(x) scanf("%d", &x)
+#define rin() freopen("in.txt", "r", stdin)
+#define wrout() freopen("out.txt", "w", stdout)
+#define fst                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Loops
+#define fr(i, s, e) for (int i = s; i < e; i++)
+#define ifr(i, s, e) for (int i = s; i <= e; i++)
+#define rfr(i, e, s) for (int i = e - 1; i >= s; i--)
+
+// Functions
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+int Set(int N, int pos) { return N = N | (1 << pos); }
+int reset(int N, int pos) { return N = N & ~(1 << pos); }
+bool check(int N, int pos) { return (bool)(N & (1 << pos)); }
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+
+// Const
+const int mod = 1000000007;
+// 28722900390631
+const int inf = 1e18 + 19;
+const int MX = 400015;
+const int pi = acos(-1.0);
+const int seed = 997;
+// 769 919 647 839
+si g[MX];
+
+void solve()
+{
+    int n, k;
+    sid(n);
+    sid(k);
+
+    fr(i, 0, n - 1)
+    {
+        int a, b;
+        sid(a);
+        sid(b);
+
+        g[a].insert(b);
+        g[b].insert(a);
+    }
+
+    vi cur;
+    vector<bool> isDel(n + 3, false);
+    ifr(i, 1, n)
+    {
+        if (g[i].size() <= 1)
+        {
+            cur.pb(i);
+        }
+    }
+
+    fr(i, 0, k)
+    {
+        si onThis;
+        vi _cur;
+        for (auto u : cur)
+        {
+            for (auto v : g[u])
+            {
+                g[v].erase(u);
+                onThis.insert(v);
+            }
+
+            g[u].clear();
+            isDel[u] = true;
+        }
+
+        for (auto u : onThis)
+        {
+            if ((int)g[u].size() <= 1 && !isDel[u])
+            {
+                _cur.pb(u);
+            }
+        }
+
+        cur = _cur;
+
+        if (cur.size() == 0)
+        {
+            break;
+        }
+    }
+
+    int ans = 0;
+    ifr(i, 1, n)
+    {
+        if (!isDel[i])
+        {
+            ans++;
+            // cout << i << " ";
+        }
+
+        g[i].clear();
+    }
+
+    printf("%d\n", ans);
+}
+
+int32_t main()
+{
+    // rin();
+    // wrout();
+    // fst;
+
+    int t;
+    sid(t);
+
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
