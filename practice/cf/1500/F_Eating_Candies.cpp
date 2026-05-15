f3f5e56f64c0bc241957226c5352530faef7c333
Tamjid Jasir <tamjid.jasir@optimizely.com>
2026-05-15 22:15:05 +0600

    Add implementation for F_Eating_Candies problem

---
+++ practice/cf/1500/F_Eating_Candies.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Data type
+#define int long long
+#define ll long long
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pii>
+#define mii map<int, int>
+#define msi map<string, int>
+#define si set<int>
+#define spi set<pii>
+
+// Method or var shorthands
+#define fi first
+#define se second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define sort_all(v) sort(all(v));
+#define rev_all(v) reverse(all(v));
+#define mem(ara, x) memset(ara, x, sizeof ara)
+
+
+
+// Input Output
+#define sild(x) scanf("%lld", &x)
+#define sid(x) scanf("%d", &x)
+#define rin() freopen("in.txt", "r", stdin)
+#define wrout() freopen("out.txt", "w", stdout)
+#define fst                       \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);                   \
+	cout.tie(0);
+
+// Loops
+#define fr(i, s, e) for (int i = s; i < e; i++)
+#define ifr(i, s, e) for (int i = s; i <= e; i++)
+#define rfr(i, e, s) for (int i = e - 1; i >= s; i--)
+
+// Functions
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+int Set(int N, int pos) { return N = N | (1 << pos); }
+int reset(int N, int pos) { return N = N & ~(1 << pos); }
+bool check(int N, int pos) { return (bool)(N & (1 << pos)); }
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+
+// Const
+const int mod = 1000000007;
+// 28722900390631
+const int inf = 1e18 + 19;
+const int MX = 200015;
+const int pi = acos(-1.0);
+const int seed = 997;
+// 769 919 647 839
+
+int w[MX], f[MX], b[MX];
+
+int32_t main()
+{
+    // rin();
+    // wrout();
+    fst;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n; cin >> n;
+
+        f[0] = b[n + 1] = 0;
+        ifr(i, 1, n) cin >> w[i];
+        ifr(i, 1, n) f[i] = f[i - 1] + w[i];
+        rfr(i, n + 1, 1) b[i] = b[i + 1] + w[i];
+
+        mii mp;
+        ifr(i, 1, n) mp[f[i]] = i;
+        int ans = 0;
+        rfr(i, n + 1, 1) {
+            mp[f[i]] = 0;;
+            if (mp[b[i]]) {
+                ans = max((n - i + 1) + mp[b[i]], ans);
+            }
+        }
+
+        cout << ans << endl;
+    }
+    return 0;
+}
