f74a4beb94e538818b20dd3ef882c7f8e7c5a6ef
prince_of_crows <tamjidjasir@gmail.com>
2026-06-20 03:07:19 +0600

    Add implementation for C_Interesting_Sequence problem

---
+++ practice/cf/1600/C_Interesting_Sequence.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Data type
+#define int long long
+#define ll long long
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define vi vector<int>
+#define vll vector<long long>
+#define vpi vector<pii>
+#define mii map<int, int>
+#define msi map<string, int>
+#define si set<int>
+#define spi set<pii>
+
+// Method or var shorthands
+#define fi first
+#define se second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define sort_all(v) sort(all(v));
+#define rev_all(v) reverse(all(v));
+#define mem(ara, x) memset(ara, x, sizeof ara)
+
+// Input Output
+#define sild(x) scanf("%lld", &x)
+#define sid(x) scanf("%d", &x)
+#define rin() freopen("in.txt", "r", stdin)
+#define wrout() freopen("out.txt", "w", stdout)
+#define fst                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Loops
+#define fr(i, s, e) for (int i = s; i < e; i++)
+#define ifr(i, s, e) for (int i = s; i <= e; i++)
+#define rfr(i, e, s) for (int i = e - 1; i >= s; i--)
+
+// Functions
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+int Set(int N, int pos) { return N = N | (1LL << pos); }
+int reset(int N, int pos) { return N = N & ~(1LL << pos); }
+bool check(int N, int pos) { return (bool)(N & (1LL << pos)); }
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+
+// Const
+const int mod = 1000000007;
+// 28722900390631
+const int inf = 1e18 + 19;
+const int MX = 200015;
+const int pi = acos(-1.0);
+const int seed = 997;
+// 769 919 647 839
+
+int32_t main()
+{
+    // rin();
+    // wrout();
+    fst;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+
+        int ans = 0, maxIdx = -1, did = -1;
+        fr(i, 0, 62)
+        {
+            if (check(n, i))
+            {
+                if (check(x, i))
+                {
+                    ans = Set(ans, i);
+
+                    if (did == -1)
+                    {
+                        did = i;
+                    }
+                }
+                else
+                {
+                    maxIdx = i;
+                }
+            }
+            else
+            {
+                if (check(x, i))
+                {
+                    cout << -1 << endl;
+                    goto done;
+                }
+            }
+        }
+
+        if (maxIdx != -1)
+        {
+            bool can = false;
+            ifr(i, maxIdx + 1, 62)
+            {
+
+                if (!check(n, i))
+                {
+                    if (!check(x, i))
+                    {
+                        if (did == -1 || did > i)
+                        {
+                            ans = Set(ans, i);
+                            can = true;
+                            break;
+                        }
+                        else
+                        {
+                            cout << -1 << endl;
+                            goto done;
+                        }
+                    }
+                }
+            }
+
+            if (!can)
+            {
+                cout << -1 << endl;
+                goto done;
+            }
+        }
+
+        cout << ans << endl;
+
+    done:;
+    }
+    return 0;
+}
